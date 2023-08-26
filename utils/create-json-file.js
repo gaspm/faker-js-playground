b8ce25f8dbc296f46a464f54b612f1341b234a1f
Michal Gašparík <michal.gasparik@zdruzeniedeus.sk>
2023-08-26 18:23:32 +0200

    Update Format, Suffix

--- utils/create-json-file.js
+++ utils/create-json-file.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 
 export function createJsonFile(data, filename) {
+
     const directoryPath = './mockup-data';
+    const suffix = 'json';
+
     if (!fs.existsSync(directoryPath)) {
-        // Create the directory
         fs.mkdirSync(directoryPath);
     }
-    const jsonData = JSON.stringify(data, null, 2); // Convert data to JSON string with 2-space indentation
-    fs.writeFileSync(`${directoryPath}/${filename}.json`, jsonData, 'utf-8');
-    console.log(`The file ${filename}.json was successfully created!`);
+
+    const jsonData = JSON.stringify(data, null, 2);
+
+    fs.writeFileSync(`${directoryPath}/${filename}.${suffix}`, jsonData, 'utf-8');
+
+    console.log(`The file ${filename}.${suffix} was successfully created!`);
+
 }
